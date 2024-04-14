965f599b4e9608ff1587653600135b5584cd2080
ravikant.jha@voizo.ai <ravikant.jha@voizo.ai>
2024-04-14 12:31:52 +0530

    Added trailer video for selected movie

--- src/components/ShowTrailer.jsx
+++ src/components/ShowTrailer.jsx
@@ -1,11 +1,46 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { useLocation } from "react-router-dom";
+import { API_OPTIONS } from "../utils/constants";
 
 const ShowTrailer = () => {
   const location = useLocation();
-  console.log(location.state.movie);
+  const selectedMovie = location.state.movie;
+  const [trailerKey, setTrailerKey] = useState();
 
-  return <div>Show Trailer</div>;
+  async function getSelectedTrailer() {
+    const data = await fetch(
+      `https://api.themoviedb.org/3/movie/${selectedMovie.id}/videos?language=en-US`,
+      API_OPTIONS
+    );
+    const json = await data.json();
+
+    const filterData = json.results.filter((video) => video.type === "Trailer");
+    const trailer = filterData.length ? filterData[0] : json.results[0];
+    setTrailerKey(trailer.key);
+  }
+
+  useEffect(() => {
+    getSelectedTrailer();
+  }, []);
+
+  return (
+    <div>
+      {trailerKey && (
+        <iframe
+          src={
+            "https://www.youtube.com/embed/" +
+            trailerKey +
+            "?&autoplay=1&mute=1"
+          }
+          title="YouTube video player"
+          frameborder="0"
+          allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+          referrerpolicy="strict-origin-when-cross-origin"
+          allowfullscreen
+        ></iframe>
+      )}
+    </div>
+  );
 };
 
 export default ShowTrailer;
